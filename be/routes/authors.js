0d9c270df260a3b3686384db608e864f529cad18
ciusi <ciuffetellisilvia@gmail.com>
2024-06-08 20:22:03 +0200

    Commit collezione Postman per le API del progetto Strive Blog

--- be/routes/authors.js
+++ be/routes/authors.js
@@ -1,11 +1,18 @@
-// routes/authors.js
-
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const Author = require('../models/authors');
 
+// Middleware per verificare l'ObjectId
+function checkObjectId(req, res, next) {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ message: 'Invalid ID' });
+  }
+  next();
+}
+
 // Rotta per ottenere la lista degli autori
-router.get('/authors', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const authors = await Author.find();
     res.json(authors);
@@ -15,12 +22,12 @@ router.get('/authors', async (req, res) => {
 });
 
 // Rotta per ottenere un singolo autore
-router.get('/authors/:id', getAuthor, (req, res) => {
+router.get('/:id', checkObjectId, getAuthor, (req, res) => {
   res.json(res.author);
 });
 
 // Rotta per creare un nuovo autore
-router.post('/authors', async (req, res) => {
+router.post('/', async (req, res) => {
   const author = new Author({
     nome: req.body.nome,
     cognome: req.body.cognome,
@@ -34,6 +41,46 @@ router.post('/authors', async (req, res) => {
     res.status(201).json(newAuthor);
   } catch (err) {
     res.status(400).json({ message: err.message });
+  }
+});
+
+// Rotta per modificare un autore
+router.put('/:id', checkObjectId, getAuthor, async (req, res) => {
+  if (req.body.nome != null) {
+    res.author.nome = req.body.nome;
+  }
+  if (req.body.cognome != null) {
+    res.author.cognome = req.body.cognome;
+  }
+  if (req.body.email != null) {
+    res.author.email = req.body.email;
+  }
+  if (req.body.data_di_nascita != null) {
+    res.author.data_di_nascita = req.body.data_di_nascita;
+  }
+  if (req.body.avatar != null) {
+    res.author.avatar = req.body.avatar;
+  }
+
+  try {
+    const updatedAuthor = await res.author.save();
+    res.json(updatedAuthor);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Rotta per cancellare un autore
+router.delete('/:id', checkObjectId, async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    if (author == null) {
+      return res.status(404).json({ message: 'Autore non trovato' });
+    }
+    await Author.deleteOne({ _id: req.params.id });
+    res.json({ message: 'Autore cancellato' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 });
 
